ac3cf2f00960f73fa57a718486dfffe24e8e504f
PAUL ADEBISI OYATOWO <106627306+pennio-design@users.noreply.github.com>
2026-01-22 00:00:20 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+    'import.meta.env.VITE_GEMINI_API_KEY': JSON.stringify(process.env.VITE_GEMINI_API_KEY)
   },
   build: {
     outDir: 'dist',
