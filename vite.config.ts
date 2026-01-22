eb4b05a2cee0616da3eb3cbeeb8c4c7170ae4227
PAUL ADEBISI OYATOWO <106627306+pennio-design@users.noreply.github.com>
2026-01-22 05:20:26 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/KnowledgeGraph/', // Add this line!
   plugins: [react()],
   define: {
     'import.meta.env.VITE_GEMINI_API_KEY': JSON.stringify(process.env.VITE_GEMINI_API_KEY)
